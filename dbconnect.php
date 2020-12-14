bda9bb0b4dc7b25bb3f8ada26adff6537f613452
Susmit Patil <susmit410@gmail.com>
2020-12-14 12:07:03 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
 define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'bookstore'); 
-define('DB_USER','root'); 
-define('DB_PASSWORD','Pradyumn'); 
+define('DB_NAME', 'id15664741_bookstore'); 
+define('DB_USER','id15664741_root'); 
+define('DB_PASSWORD',')x]2K_<JZD8<N0#3'); 
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
